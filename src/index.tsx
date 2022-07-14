556d0f31ffabcf0014ca8e2cbbadcd2bdd14e9b9
kaireewu <kaireewu@gmail.com>
2022-07-14 16:41:20 +0800

    build(react): upgrade to react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const el = document.getElementById('root');
+if (el === null) {
+  throw new Error('root container missing in index.html');
+}
+const root = createRoot(el);
+root.render(<StrictMode><App /></StrictMode>);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
